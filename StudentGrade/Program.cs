dcc0f99e3018f5f79c43a6c31a72c8335835585b
Alfi786 <alfiyasubair5@gmail.com>
2026-06-19 18:43:39 +0530

    updated program.cs

--- StudentGrade/Program.cs
+++ StudentGrade/Program.cs
@@ -1,5 +1,7 @@
+using System;
 using StudentGrade.Extension;
-
+using StudentGrade.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,15 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddApplicationServices(builder.Configuration);
 
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -29,5 +28,12 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Student}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<GradeAppDbContext>();
+    db.Database.Migrate();
+    
+}
 
 app.Run();
